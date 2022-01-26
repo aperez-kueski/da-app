9f97cc205011a33c656c994b1f88dfae3e358813
Angel Perez <angel.perez@kueski.com>
2022-01-26 13:48:45 -0600

    Package over Git enable

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:da_lib/lib.dart';
 
 void main() {
   runApp(const MyApp());
@@ -95,6 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            ContainerText(),
             const Text(
               'You have pushed the button this many times:',
             ),
